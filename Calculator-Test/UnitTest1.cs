d8db5fd80ffc55e430925ce5fc0cffb475a59d79
185august <augusteide98@hotmail.com>
2025-06-20 11:13:40 +0200

    updates

--- Calculator-Test/UnitTest1.cs
+++ Calculator-Test/UnitTest1.cs
@@ -1,58 +1,53 @@
 ﻿using Calculator_Console;
 
-namespace Calculator_Test
-{
-    public class Tests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+namespace Calculator_Test;
 
-        [Test]
-        public void TestAddition()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Addition(4, 5), Is.EqualTo(9));
-        }
-        [Test]
-        public void TestSubtraction()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Subtract(10, 5), Is.EqualTo(5));
-        }
-        [Test]
-        public void TestMultiplication()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Multiply(4, 5), Is.EqualTo(20));
-        }
-        [Test]
-        public void TestDivision()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Divide(10, 2), Is.EqualTo(5));
-        }
-        [Test]
-        public void TestDivisionByZero()
-        {
-            var calculator = new Calculator();
-            Assert.Throws<ArgumentException>(() => calculator.Divide(10, 0));
-        }
-        [Test]
-        public void TestNegativeNumbers()
-        {
-            var calculator = new Calculator();
-            Assert.That(calculator.Addition(-4, -5), Is.EqualTo(-9));
-            Assert.That(calculator.Subtract(-10, -5), Is.EqualTo(-5));
-            Assert.That(calculator.Multiply(-4, -5), Is.EqualTo(20));
-            Assert.That(calculator.Divide(-10, -2), Is.EqualTo(5));
-        }
-        [Test]
-        public void TestNegativeNumbersByZero()
-        {
-            var calculator = new Calculator();
-            Assert.Throws<ArgumentException>(() => calculator.Divide(-10, 0));
-        }
+public class Tests
+{
+    private Calculator _calculator;
+    [SetUp]
+    public void Setup()
+    {
+        _calculator = new Calculator();
+    }
+
+    [Test]
+    public void TestAddition()
+    {
+        Assert.That(_calculator.Addition(4, 5), Is.EqualTo(9));
+    }
+    [Test]
+    public void TestSubtraction()
+    {
+            
+        Assert.That(_calculator.Subtract(10, 5), Is.EqualTo(5));
+    }
+    [Test]
+    public void TestMultiplication()
+    {
+        Assert.That(_calculator.Multiply(4, 5), Is.EqualTo(20));
+    }
+    [Test]
+    public void TestDivision()
+    {
+        Assert.That(_calculator.Divide(10, 2), Is.EqualTo(5));
+    }
+    [Test]
+    public void TestDivisionByZero()
+    {
+        Assert.Throws<ArgumentException>(() => _calculator.Divide(10, 0));
+    }
+    [Test]
+    public void TestNegativeNumbers()
+    {
+        Assert.That(_calculator.Addition(-4, -5), Is.EqualTo(-9));
+        Assert.That(_calculator.Subtract(-10, -5), Is.EqualTo(-5));
+        Assert.That(_calculator.Multiply(-4, -5), Is.EqualTo(20));
+        Assert.That(_calculator.Divide(-10, -2), Is.EqualTo(5));
+    }
+    [Test]
+    public void TestNegativeNumbersByZero()
+    {
+        Assert.Throws<ArgumentException>(() => _calculator.Divide(-10, 0));
     }
 }
